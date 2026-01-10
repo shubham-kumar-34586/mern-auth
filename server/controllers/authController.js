91f9da7ae5fcae4b8b11ef7ee44d58bee7d544a0
Shubham Kumar <shubhamkumar34586@gmail.com>
2026-01-10 21:59:31 +0530

    connected with Brevo and message coming correctly

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js'
 
 export const register = async (req, res)=>{
     const {name, email, password} = req.body;
@@ -29,7 +30,15 @@ export const register = async (req, res)=>{
              sameSite: process.env.NODE_ENV === 'production' ?
              'none' : 'strict',
              maxAge: 7 * 24 * 60 * 60 * 1000
-            })
+            });
+            // Sending Welcome email
+            const maileOptions = {
+                from: process.env.SENDER_EMAIL,
+                to: email,
+                subject: 'Welcome to GreatStack',
+                text:`Welcome to gratstack website. Your account has been created with email id: ${email}`
+            }
+            await transporter.sendMail(maileOptions);
 
             return res.json({success: true});
 
@@ -93,3 +102,4 @@ export const logout = async (req, res)=>{
          return res.json({ success: false, message: error.message });
     }
 }
+
